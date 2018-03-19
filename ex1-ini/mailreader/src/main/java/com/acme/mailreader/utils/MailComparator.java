d05f4fa059ebe1a59cb806a15a71af5c2b57e257
FrapperColin <colin.frapper@gmail.com>
2018-03-19 22:41:24 +0100

    Some changes exercice 1

--- ex1-ini/mailreader/src/main/java/com/acme/mailreader/utils/MailComparator.java
+++ ex1-ini/mailreader/src/main/java/com/acme/mailreader/utils/MailComparator.java
@@ -10,29 +10,73 @@ import com.acme.mailreader.model.Mail;
  * Comme on désire afficher les mails les plus importants en premier, l'element le plus grand retourne une valeur négative
  *
  */
-// D�gager tous ces commentaires
+// D�gager tous ces commentaires
 public class MailComparator implements Comparator<Mail> {
 
-	public int compare(Mail obj1, Mail obj2) {
-		if (obj1 == null || obj2 == null) {
-			return 0;
+    final int LESS_IMPORTANT = 1;
+    final int EQUAL = 0;
+    int MORE_IMPORTANT = -1;
+    
+	public int compare(Mail mail, Mail otherMail) {
+		
+//		if (mail == null || otherMail == null) {
+//			return EQUAL;
+//		}
+		
+		if (mail == null || otherMail == null) {
+			throw new IllegalArgumentException("Can't compare with a null value");
 		}
-		if (obj1.isImportant() != obj2.isImportant()) {
-			if (obj1.isImportant() && !obj2.isImportant()) {
-				return -1;
+		
+		
+		
+		
+//		original
+		if (mail.isImportant() != otherMail.isImportant()) {
+			if (mail.isImportant() && !otherMail.isImportant()) {
+				return MORE_IMPORTANT;
 			} else {
-				return 1;
+				return LESS_IMPORTANT;
 			}
 		}
-		if (obj1.getStatut() != obj2.getStatut()) {
-			int comp = obj1.getStatut().ordinal()
-					- obj2.getStatut().ordinal();
-			return comp > 0 ? -1 : 1;
+		
+
+		// original
+//		if (mail.getStatut() != otherMail.getStatut()) {
+//			int comp = mail.getStatut().ordinal()
+//					- otherMail.getStatut().ordinal();
+//			return comp > 0 ? -1 : 1;
+//		}
+		
+		// utiliser des méthodes courtes page 90 livre
+		
+		if (!(mail.getStatut().equals(otherMail.getStatut()))) {
+			int compareOrder = compareOrderStatus(mail,otherMail);
+			if(compareOrder > 0) {
+				return MORE_IMPORTANT ;
+			} else {
+				return LESS_IMPORTANT;
+			}
 		}
-		if (obj1.getSujet() != obj2.getSujet()) {
-			return obj2.getSujet().compareTo(obj1.getSujet());
+		
+		
+		
+//		if (mail.getSujet() != otherMail.getSujet()) {
+//			return otherMail.getSujet().compareTo(mail.getSujet());
+//		}
+		
+		if (!(mail.getSujet().equals(otherMail.getSujet()))) {
+			return compareMailSubjet(mail,otherMail) ;
 		}
-		return obj2.getDate().compareTo(obj1.getDate());
+		
+		return otherMail.getDate().compareTo(mail.getDate());
+	}
+
+	private int compareMailSubjet(Mail mail, Mail otherMail) {
+		return otherMail.getSujet().compareTo(mail.getSujet());
+	}
+
+	private int compareOrderStatus(Mail mail, Mail otherMail) {
+		return mail.getStatut().ordinal() - otherMail.getStatut().ordinal();
 	}
 	
 
